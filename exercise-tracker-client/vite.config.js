b8331f695b9cd6d85e8214b4ddbb94eceaa41eaf
Fredrickmureti <fredrickmureti612@gmail.com>
2024-07-14 22:02:35 +0300

    debugging

--- exercise-tracker-client/vite.config.js
+++ exercise-tracker-client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-exercise-tracker-seven.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
