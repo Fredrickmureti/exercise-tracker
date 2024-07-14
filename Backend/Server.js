7f48110d4e2bb5686cf85a8fc1f3baf9058cb86d
Fredrickmureti <fredrickmureti612@gmail.com>
2024-07-14 22:40:14 +0300

    debugging

--- Backend/Server.js
+++ Backend/Server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -52,6 +51,4 @@ app.use((req, res, next) => {
   res.status(404).send('404: Not Found');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
